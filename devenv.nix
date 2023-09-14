e00f947c4e3e33e363cf40ed60cebed2b9b7bcab
Sander <hey@sandydoo.me>
2023-09-14 19:58:09 +0300

    Test

---
+++ devenv.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.hello ];
+
+  enterShell = ''
+    echo "Entered shell"
+  '';
+}
